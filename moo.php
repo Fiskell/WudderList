10b11fe752e923547de16ec804623e0a7707f2e0
Fiskell <stephen.fiskell@gmail.com>
2015-12-25 22:18:58 -0800

    Add more testing

--- moo.php
+++ moo.php
@@ -7,10 +7,12 @@ $dotenv = new Dotenv\Dotenv(__DIR__);
 $dotenv->load();
 
 //(new \WudderList\WunderList())->getLists();
+$baseCommand = new BaseCommand();
 while(true) {
-    $wunderlist = (new \WudderList\WunderList());
-    $taskId = $wunderlist->getFirstPosition(210225917);
-    $task = $wunderlist->getTask($taskId);
-    (new BaseCommand())->cowsay($task['title']);
+//    $wunderlist = (new \WudderList\WunderList());
+//    $taskId = $wunderlist->getFirstPosition(210225917);
+//    $task = $wunderlist->getTask($taskId);
+//    $baseCommand->cowsay($task['title']);
+    $baseCommand->cowsay('yessss');
     sleep(10);
 }
