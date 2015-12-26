c334b8abef44a399e0b6c44a3f56069d4cebb62f
Fiskell <stephen.fiskell@gmail.com>
2015-12-25 20:43:39 -0800

    Add loading of .env to moo

--- moo.php
+++ moo.php
@@ -3,9 +3,11 @@ use WudderList\Command\BaseCommand;
 
 require __DIR__.'/vendor/autoload.php';
 
+$dotenv = new Dotenv\Dotenv(__DIR__);
+$dotenv->load();
+
 $count = 0;
 while(true) {
-
     (new BaseCommand())->cowsay('a' . $count);
     sleep(2);
     $count++;
